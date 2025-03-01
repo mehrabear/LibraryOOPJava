2bfae604edac94fd942cd390c8bad27653312aa3
mehrabear~ <mehrad345@gmail.com>
2025-03-01 10:31:47 +0330

    Thesis creation

---
+++ Thesis.java
@@ -0,0 +1,55 @@
+public class Thesis {
+    private final String id;
+    private String title;
+    private String studentName;
+    private String professorName;
+    private int year;
+    private Category category;
+    private boolean borrowed;
+
+    public Thesis(String id, String title, String studentName, String professorName, int year, Category category){
+        this.id = new String(id);
+        this.title = new String(title);
+        this.studentName = new String(studentName);
+        this.professorName = new String(professorName);
+        this.year = year;
+        this.category = category;
+        borrowed = false;
+    }
+
+    public String getId(){
+        return new String(this.id);
+    }
+
+    public void setTitle(String title){
+        this.title = new String(title);
+    }
+
+    public void setStudentName(String studentName){
+        this.studentName = new String(studentName);
+    }
+
+    public void setProfessorName(String professorName){
+        this.professorName = new String(professorName);
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public void setCategory(String categoryId){
+        if(Category.categoryExist(categoryId)){
+            this.category = Category.catssearch(categoryId);
+        }
+    }
+
+    public boolean getBorrowed(){
+        return this.borrowed;
+    }
+
+
+
+
+
+
+}
